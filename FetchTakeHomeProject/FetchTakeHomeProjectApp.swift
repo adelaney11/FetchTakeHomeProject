d7bbc320bc3ea8ed1fe258b4416633559568c781
adelaney11 <adam.delaney11@gmail.com>
2025-02-24 22:00:36 -0800

    Initial Commit

---
+++ FetchTakeHomeProject/FetchTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeProjectApp.swift
+//  FetchTakeHomeProject
+//
+//  Created by Adam Delaney on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
